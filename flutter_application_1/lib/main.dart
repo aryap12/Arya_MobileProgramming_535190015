3cae2183fe6e698a427c9257a0e080baf6c0737a
aryap12 <70554757+aryap12@users.noreply.github.com>
2021-02-22 17:25:43 +0700

    nih

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -72,7 +72,7 @@ class _HomePageState extends State<HomePage> {
               ? Text("Pokeapp")
               : TextField(
                   onSubmitted: (value) {
-                    setState(() {
+                    setState(() async {
                       key = value;
                       Navigator.push(
                           context,
